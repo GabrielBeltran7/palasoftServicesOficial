72cdcb352aedbc67bb296935a62e605d141b87e2
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-11-04 18:13:45 -0500

    creando menu amburguesa

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa"; /* Importa el ícono de hamburguesa */
+import { FaBars } from "react-icons/fa";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
@@ -8,6 +8,10 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
   };
 
   return (
@@ -18,21 +22,69 @@ const Navbar = () => {
           alt="henryLogo"
         />
       </Link>
-      <FaBars
-        className={`menu-icon ${showMenu ? 'active' : ''}`}
-        onClick={toggleMenu}
-      />
-      <div className={`linkcontainer ${showMenu ? "active" : ""}`}>
-        <Link to="/landing">Inicio</Link>
-        <Link to="/home">Servicios</Link>
-        <Link to="/proyectos">Proyectos</Link>
-        <Link to="/contactenos">Contactenos</Link>
-        <Link to="/colaboradores">Colaboradores</Link>
+      <div className={`${styles.menuicono} ${showMenu ? styles.active : ""}`} onClick={toggleMenu}>
+        <FaBars />
+      </div>
+      <div className={`${styles.linkcontainer} ${showMenu ? styles.active : ""}`}>
+        <Link to="/landing" onClick={closeMenu}>
+          Inicio
+        </Link>
+        <Link to="/home" onClick={closeMenu}>
+          Servicios
+        </Link>
+        <Link to="/proyectos" onClick={closeMenu}>
+          Proyectos
+        </Link>
+        <Link to="/contactenos" onClick={closeMenu}>
+          Contactenos
+        </Link>
+        <Link to="/colaboradores" onClick={closeMenu}>
+          Colaboradores
+        </Link>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { FaBars } from "react-icons/fa";
+// import styles from "./Navbar.module.css"; // Asegúrate de que styles esté importado correctamente
+
+// const Navbar = () => {
+//   const [showMenu, setShowMenu] = useState(false);
+
+//   const toggleMenu = () => {
+//     console.log("Toggle menu called");
+//     setShowMenu(!showMenu);
+//   };
+
+//   return (
+//     <div className={styles.navcontainer}>
+//       <Link to="/">
+//         <img
+//           src="https://firebasestorage.googleapis.com/v0/b/link-6eb7d.appspot.com/o/innovacionDigitallogo.png?alt=media&token=9822d711-4f1f-4a9c-9852-0cf7cf715738"
+//           alt="henryLogo"
+//         />
+//       </Link>
+//       <div className={`${styles.menuicono} ${showMenu ? styles.active : ""}`} onClick={toggleMenu}>
+//         <FaBars />
+//       </div>
+//       <div className={`${styles.linkcontainer} ${showMenu ? styles.active : ""}`}>
+//         <Link to="/landing">Inicio</Link>
+//         <Link to="/home">Servicios</Link>
+//         <Link to="/proyectos">Proyectos</Link>
+//         <Link to="/contactenos">Contactenos</Link>
+//         <Link to="/colaboradores">Colaboradores</Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
 
 
